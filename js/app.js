83c06d9ea1d9e57b4cd28aa8c2b40bdc92dcaeb0
githamm <khamm@denverpost.com>
2018-11-21 16:10:27 -0700

    Fix errors due to libraries loading via http

--- js/app.js
+++ js/app.js
@@ -218,8 +218,8 @@ var drowningIcon = L.divIcon({
                 iconSize: [12, 12]
 });
 
-L.tileLayer('http://{s}.{base}.maps.cit.api.here.com/maptile/2.1/{type}/{mapID}/reduced.day/{z}/{x}/{y}/{size}/{format}?app_id={app_id}&app_code={app_code}&lg={language}', {
-    attribution: 'Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>',
+L.tileLayer('//{s}.{base}.maps.cit.api.here.com/maptile/2.1/{type}/{mapID}/reduced.day/{z}/{x}/{y}/{size}/{format}?app_id={app_id}&app_code={app_code}&lg={language}', {
+    attribution: 'Map &copy; 1987-2014 <a href="//developer.here.com">HERE</a>',
     subdomains: '1234',
     mapID: 'newest',
     app_id: '2K8wqfYF2CuD5D67HdQx',
